fba0dacab825e9ac46d40183730fa76891de65b6
John Gregg <greggj2016@gmail.com>
2022-03-10 17:58:22 -0500

    commented out an operation for testing in geno_sim_library.py

--- geno_sim_library.py
+++ geno_sim_library.py
@@ -106,8 +106,11 @@ def simulate_correlated_SNPs(mafs, R_vec, N):
     Z = np.random.multivariate_normal(np.zeros(len(mafs)), cov_z, N)
     U = norm.cdf(Z)
     X = np.array([F_set[i].inv_cdf(U[:, i]) for i in range(len(F_set))])
+    print(np.corrcoef(X))
     return(X.T)
 
-#mafs = [0.5, 0.375, 0.25, 0.2]
-#R_vec = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-#simulate_correlated_SNPs(mafs, R_vec, 1000000)
+'''
+mafs = [0.5]*10
+R_vec = [0.5]*np.sum(np.arange(10)) 
+simulate_correlated_SNPs(mafs, R_vec, 10000000)
+'''
