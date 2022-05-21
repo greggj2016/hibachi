Fixed a bug where the variance becomes 0 if the function, by chance, only has three variables, which only allows for one IG3 term, which makes the variance 0 because one term has 0 variance. This would stop the penalty from doing it's job. Now, in such cases (including cases where the number of IG3 terms is less than the desired number of terms). 0 values are appended to the vector that is input for the variance function to ensure that the penalty works properly.

IO.py
@@ -65,6 +65,7 @@ def get_arguments():
     parser.add_argument('-e', '--evaluation', type=str,
             help='name of evaluation [normal|folds|subsets|noise|oddsratio]' +
                  ' (default=normal) note: oddsratio sets columns == 10')
+    parser.add_argument('-effs', '--num_effects', type=int, help = 'number of effects that you want to simulate')
     parser.add_argument('-f', '--file', type=str,
             help='name of training data file (REQ)' +
                  ' filename of random will create all data')
@@ -154,6 +155,11 @@ def get_arguments():
     else:
         print("exiting: unrecognized value for -python_scoop argument")
         exit()
+
+    if(args.num_effects == None):
+        options['num_effects'] = 1
+    else:
+        options['num_effects'] = args.num_effects
 
     if(args.njobs == None):
         options['njobs'] = 1
@@ -291,10 +297,12 @@ def read_file(fname):
 ###############################################################################
 def write_model(outfile, best):
     """ write top individual out to model file """
-    f = open(outfile, 'w')
-    f.write(str(best[0]))
-    f.write('\n')
-    f.close()
+    functions = [str(b) for b in best]
+    penalized_igs = [b.fitness.values[0] for b in best]
+    lengths = [b.fitness.values[1] for b in best]
+    model_df = pd.DataFrame(np.array([penalized_igs, lengths, functions]).T)
+    model_df.columns = ["penalized_ig", "length", "model"]
+    model_df.to_csv(outfile, sep = "\t", header = True, index = False)
 ###############################################################################
 def read_model(infile):
     f = open(infile, 'r')

hib.py
@@ -68,6 +68,7 @@ start = time.time()
 
 maf = options['minor_allele_freq']
 cov = options['covariance_info']
+num_effects = options['num_effects']
 python_scoop = options['python_scoop']
 infile = options['file']
 try:
@@ -290,7 +291,12 @@ def evalData(individual, xdata, xtranspose):
                 ig_vals = np.array([MI[-1][0] for MI in out])
                 igsum = np.sum(ig_vals)
                 igsums = np.append(igsums, igsum)
-                igvar = np.var(np.sort(ig_vals)[-ig:])
+                if len(ig_vals) < num_effects: 
+                    all_parts = np.zeros(num_effects)
+                    all_parts[:len(ig_vals)] += ig_vals
+                    igvar = np.var(all_parts)
+                else:                   
+                    igvar = np.var(np.sort(ig_vals)[-num_effects:])
                 igvars = np.append(igvars, igvar)
 
     if evaluate == 'oddsratio':
@@ -302,6 +308,8 @@ def evalData(individual, xdata, xtranspose):
     igsum_avg = np.mean(igsums)        
     igvar_avg = np.mean(igvars)
     fit_fun = igsum_avg - lam*igvar_avg
+    #if igsum_avg > 0.5:
+    #    pdb.set_trace()
     # print(fit_fun)
     # pdb.set_trace()
     if len(individual) <= 1:
@@ -366,7 +374,7 @@ def hibachi(pop,gen,rseed,showall):
         stats.register("min", np.min, axis=0)
         stats.register("max", np.max, axis=0)
     else:
-        stats = tools.Statistics(lambda ind: max(ind.fitness.values[0],0))
+        stats = tools.Statistics(lambda ind: ind.fitness.values[0])
         stats.register("avg", np.mean)
         stats.register("std", np.std)
         stats.register("min", np.min)
@@ -416,7 +424,7 @@ if __name__ == "__main__":
     # Start evaluation here
     #
     pop, stats, hof, logbook = hibachi(population,generations,rseed,showall)
-    best = np.array([ind for ind in hof])
+    best = np.array([ind for ind in hof], dtype = object)
     fitness = np.array([b.fitness.values for b in best])
     penalized_igs, lengths = [f[0] for f in fitness], [f[1] for f in fitness]
     best_index = np.argmax(penalized_igs)
@@ -484,7 +492,7 @@ if __name__ == "__main__":
     outfile += popstr + '-'
     outfile += genstr + '-'
     outfile += str(evaluate) + "-" + 'ig' + str(ig) + "way"
-    outfile += "_lam" + str(lam) + ".txt" 
+    outfile += "_lam" + str(lam) + "_effs" + str(num_effects) + ".txt" 
 
     time4 = time.strftime("%H:%M:%S", time.localtime())
     printf("file definition end time:  %s\n", time4)
@@ -503,12 +511,12 @@ if __name__ == "__main__":
     moutfile += popstr + '-'
     moutfile += genstr + '-'
     moutfile += str(evaluate) + "-" + 'ig' + str(ig) + "way"
-    moutfile += "_lam" + str(lam) + ".txt" 
+    moutfile += "_lam" + str(lam) + "_effs" + str(num_effects) + ".txt" 
     printf("writing model to %s\n", moutfile)
     time7 = time.strftime("%H:%M:%S", time.localtime())
     printf("file2 definition end time:  %s\n", time7)
 
-    IO.write_model(moutfile, best_model)
+    IO.write_model(moutfile, best)
     time8 = time.strftime("%H:%M:%S", time.localtime())
     printf("file2 writing end time:  %s\n", time8)
 
@@ -530,7 +538,7 @@ if __name__ == "__main__":
             outfile = outdir + 'model_from-' + file1 
             outfile += '-using-' + nfile + '-' + str(rseed) + '-' 
             outfile += str(evaluate) + '-' + str(ig) + "way"
-            outfile += "_lam" + str(lam) + ".txt" 
+            outfile += "_lam" + str(lam) + "_effs" + str(num_effects) + ".txt" 
             printf("%s\n", outfile)
             IO.create_file(X,nresult,outfile)
 